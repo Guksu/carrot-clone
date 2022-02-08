e5d928986b717944a9a5bf1d625c914a81dd4b12
Guksu <yung3152@daum.net>
2022-02-09 02:24:31 +0900

    prisma / creat user

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../../libs/server/client";
 import withHandler from "../../../libs/server/withHandler";
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.json({ ok: true });
+// upsert로 간략하게 코드를 작성할 수 있다.
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Someone",
+      ...payload,
+    },
+    update: {},
+  });
+
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
